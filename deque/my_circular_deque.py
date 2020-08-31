bb0410bf21637a8c3e00df54ec3a44d8ef824379
root <didguscjf95@naver.com>
2020-08-31 23:47:13 +0900

    8.31

---
+++ deque/my_circular_deque.py
@@ -0,0 +1,105 @@
+class MyCircularDeque:
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the deque to be k.
+        """
+        self.head, self.tail = ListNode(None), ListNode(None)
+        self.maxlen = k
+        self.len = 0
+        self.head.right, self.tail.left = self.tail, self.head
+
+    def _add(self, node: ListNode, new: ListNode):
+        n = node.right
+        node.right = new
+        new.left = node
+        new.right = n
+        n.left = new
+
+    def _del(self, node: ListNode):
+        n = node.right.right
+        node.right = n
+        n.left = node
+
+    def insertFront(self, value: int) -> bool:
+        """
+        Adds an item at the front of Deque. Return true if the operation is successful.
+        """
+        if self.len == self.maxlen:
+            return False
+        self._add(self.head, ListNode(value))
+        self.len += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        """
+        Adds an item at the rear of Deque. Return true if the operation is successful.
+        """
+        if self.len == self.maxlen:
+            return False
+        self._add(self.tail.left, ListNode(value))
+        self.len += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        """
+        Deletes an item from the front of Deque. Return true if the operation is successful.
+        """
+        if self.len == 0:
+            return False
+        # delete
+        self._del(self.head)
+        self.len -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        """
+        Deletes an item from the rear of Deque. Return true if the operation is successful.
+        """
+        if self.len == 0:
+            return False
+        # delete
+        self._del(self.tail.left.left)
+        self.len -= 1
+        return True
+
+    def getFront(self) -> int:
+        """
+        Get the front item from the deque.
+        """
+        if self.len == 0:
+            return -1
+        else:
+            return self.head.right.val
+
+    def getRear(self) -> int:
+        """
+        Get the last item from the deque.
+        """
+        if self.len == 0:
+            return -1
+        else:
+            return self.tail.left.val
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular deque is empty or not.
+        """
+        return self.len == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular deque is full or not.
+        """
+        return self.len == self.maxlen
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
---
+++ stack_queue/stack_ADT.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self):
+        self.item = item
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.last = None
+
+    def push(self, item):
+        self.last = Node(item, self.last)
+
+    def pop(self):
+        item = self.last.item
+        self.last = self.last.next
+        return item
