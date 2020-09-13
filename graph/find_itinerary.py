91e687b35d0dbc54bad9b92288781ac13eb99c9c
root <didguscjf95@naver.com>
2020-09-13 23:41:54 +0900

    upgrade

--- graph/find_itinerary.py
+++ graph/find_itinerary.py
@@ -17,3 +17,23 @@ class Solution:
 
         return routes[::-1]
 
+    """
+        # by stack
+        
+        route = []
+        
+        graph = collections.defaultdict(list)
+        
+        for a,b in sorted(tickets, reverse=True):
+            graph[a].append(b)
+            
+        def dfs(a):
+            while graph[a]:
+                dfs(graph[a].pop())
+            route.append(a)
+                
+        dfs('JFK')
+        
+        return route[::-1]
+            
+    """
